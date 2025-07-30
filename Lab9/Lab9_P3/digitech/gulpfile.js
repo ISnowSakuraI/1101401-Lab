78a1948aada489dc4ee3aea49ab2dd7559330dce
SnowSakura <Diamondgametv@gmail.com>
2025-07-30 15:46:47 +0700

    fix

--- Lab9/Lab9_P3/digitech/gulpfile.js
+++ Lab9/Lab9_P3/digitech/gulpfile.js
@@ -1,10 +1,17 @@
 const { src, dest, watch, parallel } = require("gulp");
-const sass = require("gulp-sass")(require('sass')); // <-- CHANGE THIS LINE
+const gulp = require("gulp"),
+    sass = require("gulp-sass")(require("sass-embedded")),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create();
+
 const minifyCSS = require("gulp-csso");
 
 function css() {
   return src("assets/scss/*.scss")
-    .pipe(sass().on('error', sass.logError)) // Added error logging for sass
+    .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
     .pipe(dest("css"));
 }
